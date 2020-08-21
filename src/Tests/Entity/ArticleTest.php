bb0b90385cf70f1c3536973603b9a3d8f1dca42e
Houssem BOUALI <asana.team.2020@gmail.com>
2020-08-22 01:00:54 +0200

    unitTests:Symfony

---
+++ src/Tests/Entity/ArticleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+// tests/Entity/ArticleTest.php
+namespace App\Tests\Entity;
+
+use App\Entity\Article;
+use PHPUnit\Framework\TestCase;
+
+class ArticleTest extends TestCase
+{
+    public function testUri()
+    {
+        $article = new Article();
+        $uri = "Test 2";
+
+        $article->setUri($uri);
+        $this->assertEquals("test_2", $article->getUri());
+    }
+}
